1583ff6f97b1ab868b15b93e3bce8687735d7c34
bhanu-3333 <bhanuu339@gmail.com>
2026-01-22 21:53:09 +0530

    style: normalize Google icon size in auth button

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import bgImage from "../assets/image/image.png";
+import googleIcon from "../assets/image/google.png";
+import eyeOpen from "../assets/image/eye-open.png";
+import eyeClose from "../assets/image/eye-close.png";
+
 import "../styles/authLayout.css";
 import "../styles/signup.css";
 
 export default function Signup() {
   const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   return (
     <div className="auth-container">
@@ -19,13 +27,52 @@ export default function Signup() {
         <div className="form-box">
           <div className="tab1">Register</div>
           <h2 className="title">Create your account</h2>
+  <button className="google-btn">
+            <img src={googleIcon} alt="Google" className="google-icon" />
+            <span>Continue with Google</span>
+          </button>
 
-          <button className="google-btn">Continue with Google</button>
+          {/* USERNAME */}
+          <div className="input-wrapper">
+            <input className="input" placeholder="Username" />
+          </div>
 
-          <input className="input" placeholder="Username" />
-          <input className="input" type="email" placeholder="Email" />
-          <input className="input" type="password" placeholder="Password" />
-          <input className="input" type="password" placeholder="Confirm Password" />
+          {/* EMAIL */}
+          <div className="input-wrapper">
+            <input className="input" type="email" placeholder="Email" />
+          </div>
+
+          {/* PASSWORD */}
+          <div className="input-wrapper">
+            <input
+              className="input"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+            />
+            <img
+              src={showPassword ? eyeOpen : eyeClose}
+              className="input-icon clickable"
+              alt="toggle password"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          </div>
+
+          {/* CONFIRM PASSWORD */}
+          <div className="input-wrapper">
+            <input
+              className="input"
+              type={showConfirmPassword ? "text" : "password"}
+              placeholder="Confirm Password"
+            />
+            <img
+              src={showConfirmPassword ? eyeOpen : eyeClose}
+              className="input-icon clickable"
+              alt="toggle confirm password"
+              onClick={() =>
+                setShowConfirmPassword(!showConfirmPassword)
+              }
+            />
+          </div>
 
           <button className="btn">Sign up</button>
 
